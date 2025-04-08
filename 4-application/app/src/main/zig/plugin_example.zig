2c4b44a70ffffeab3807ed187837ae48a3d3cb97
Antonio Cheong <acheong@duti.dev>
2025-04-08 09:09:59 +0100

    zig: Use Packed struct instead of @intCast for reliability

--- 4-application/app/src/main/zig/plugin_example.zig
+++ 4-application/app/src/main/zig/plugin_example.zig
@@ -1,17 +1,21 @@
 const wasm_allocator = @import("std").heap.wasm_allocator;
 
 extern "wasm" fn log(ptr: [*]const u8, len: i32) void;
-extern "wasm" fn fetch(urlPtr: [*]const u8, urlLen: i32, dataPtr: [*]const u8, dataLen: i32) i64;
+extern "wasm" fn fetch(urlPtr: [*]const u8, urlLen: i32, dataPtr: [*]const u8, dataLen: i32) PackedPtr;
 extern "wasm" fn download(ptr: [*]const u8, len: i32) void;
 
-fn decode64(i: i64) [2]u32 {
-    return .{ @intCast(i >> 32), @intCast(i) };
-}
+const PackedPtr = packed struct(i64) {
+    len: u32,
+    ptr: [*]const u8,
+
+    fn slice(foo: PackedPtr) []const u8 {
+        return foo.ptr[0..foo.len];
+    }
+};
 
 export fn songOfTheDay() void {
     const url = "https://duti.dev/video.txt";
-    const resp = decode64(fetch(url, url.len, "", 0));
-    const slice = @as([*]const u8, @ptrFromInt(resp[0]))[0..@intCast(resp[1])];
+    const slice = fetch(url, url.len, "", 0).slice();
     log(slice.ptr, @intCast(slice.len));
     download(slice.ptr, @intCast(slice.len));
     dealloc(slice.ptr, @intCast(slice.len));
